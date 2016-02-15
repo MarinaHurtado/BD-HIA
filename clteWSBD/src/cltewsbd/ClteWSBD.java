763cf6a3d0a917d8e797ffd428edc2fe856686ca
sdist <sdist@CC102-14.itam.mx>
2016-02-15 14:03:50 -0600

    Crear cliente para correr el WebService strBD

---
+++ clteWSBD/src/cltewsbd/ClteWSBD.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cltewsbd;
+
+/**
+ *
+ * @author sdist
+ */
+public class ClteWSBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        System.out.println(consultAlumnos());
+    }
+
+    private static String consultAlumnos() {
+        wsbd.StrBD_Service service = new wsbd.StrBD_Service();
+        wsbd.StrBD port = service.getStrBDPort();
+        return port.consultAlumnos();
+    }
+    
+}
